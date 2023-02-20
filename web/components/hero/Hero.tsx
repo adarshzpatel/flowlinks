20ae16a54f0c76e21a8984896997ad08c04a507e
PAVITRA BEHARA <pavitraredmi@gmail.com>
2023-02-20 22:52:40 +0530

    ✅ initial setup

---
+++ web/components/hero/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <div className='text-neutral-500 text-2xl'>
+        Build Under Development 🚧
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  darkMode:'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        gray: colors.zinc,
+        'flow': {
+          100:'#CBFED4',
+          200:'#98FDB4',
+          300:'#64FA9D',
+          400:'#3DF596',
+          500:'#00ef8b',
+          600:'#00CD8C',
+          700:'#00AC86',
+          800:'#008A7A',
+          900:'#007270',
+        },
+      }
+    },
   },
   plugins: [],
 }
