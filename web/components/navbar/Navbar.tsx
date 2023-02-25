6c95a93b6a554b38622d13ed2b3d45193df99f1a
Krishna Mahato <krishhtrishh9304@gmail.com>
2023-02-25 11:53:48 +0530

    ADD: zustand setup and added global store to store base user

--- web/components/navbar/Navbar.tsx
+++ web/components/navbar/Navbar.tsx
@@ -3,53 +3,15 @@ import Link from "next/link";
 import Button from "../ui/Button";
 import { useAuth } from "../../context/AuthContext";
 import "../../flow/config";
-import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
-import { useEffect, useState } from "react";
-import { Database } from "../../utils/databse.types";
+import useBaseUser from "../../stores/useBaseUser";
+import { useEffect } from "react";
 
 //Components
 
 const Navbar = () => {
   const { currentUser, logOut, logIn } = useAuth();
-  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
 
-  const user = useUser();
-  const supabase = useSupabaseClient<Database>();
-
-  async function downloadImage(path: string) {
-    try {
-      const { data, error } = await supabase.storage
-        .from("avatars")
-        .download(path);
-      if (error) {
-        throw error;
-      }
-      const url = URL.createObjectURL(data);
-      setAvatarUrl(url);
-    } catch (error) {
-      console.log("Error downloading image: ", error);
-    }
-  }
-
-  const fetchAvatarUrl = async () => {
-    let { data, error, status } = await supabase
-      .from("profiles")
-      .select(`avatar_url`)
-      .eq("id", user?.id)
-      .single();
-
-    if (error && status !== 406) {
-      throw error;
-    }
-
-    if (data && typeof data.avatar_url === "string") {
-      downloadImage(data.avatar_url);
-    }
-  };
-
-  useEffect(() => {
-    if (user) fetchAvatarUrl();
-  }, [user]);
+  const { user } = useBaseUser();
 
   return (
     <header className="px-8 py-4 sticky top-0 backdrop-blur-xl z-50 border-b border-b-gray-800">
@@ -84,14 +46,12 @@ const Navbar = () => {
             <div className="w-10 h-10 border hover:scale-105 transform duration-150 cursor-pointer">
               <img
                 src={
-                  avatarUrl
-                    ? `${avatarUrl}`
-                    : user && user.user_metadata.avatar_url
-                    ? user.user_metadata.avatar_url
+                  user.id && user.avatar_url
+                    ? `${user.avatar_url}`
                     : "https://avatar.tobi.sh/tobiaslins.svg?text=" +
                       user?.email?.slice(0, 2)
                 }
-                alt=""
+                alt="prof.img"
               />
             </div>
           </Link>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -2,11 +2,16 @@ import type { NextPage } from "next";
 import Container from "../layouts/Container";
 // import Test from "../components/Test";
 import { useEffect, useState } from "react";
-import { getAllOwners, mintFlowlink } from "../flow/scripts";
+import { getAllOwners } from "../flow/scripts";
 import { useAuth } from "../context/AuthContext";
 import Link from "next/link";
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
-import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import {
+  useSession,
+  useSupabaseClient,
+  useUser,
+} from "@supabase/auth-helpers-react";
+import useBaseUser from "../stores/useBaseUser";
 
 const Home: NextPage = () => {
   const { currentUser } = useAuth();
@@ -16,6 +21,55 @@ const Home: NextPage = () => {
 
   const session = useSession();
   const supabase = useSupabaseClient();
+
+  const user = useUser();
+
+  const { setUser } = useBaseUser();
+
+  async function downloadImage(path: string) {
+    try {
+      const { data, error } = await supabase.storage
+        .from("avatars")
+        .download(path);
+      if (error) {
+        throw error;
+      }
+      const url = URL.createObjectURL(data);
+      setUser({ avatar_url: url });
+    } catch (error) {
+      console.log("Error downloading image: ", error);
+    }
+  }
+
+  const fetchAndSetAvatarUrl = async () => {
+    let { data, error, status } = await supabase
+      .from("profiles")
+      .select(`avatar_url`)
+      .eq("id", user?.id)
+      .single();
+
+    if (error && status !== 406) {
+      throw error;
+    }
+
+    if (data && typeof data.avatar_url === "string") {
+      downloadImage(data.avatar_url);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      setUser({
+        id: user.id,
+        username: user.user_metadata.username,
+        avatar_url: user.user_metadata.avatar_url,
+        email: user.email,
+        full_name: user.user_metadata.full_name,
+        website: user.user_metadata.website,
+      });
+      fetchAndSetAvatarUrl();
+    }
+  }, [user]);
 
   useEffect(() => {
     if (loggedIn)
